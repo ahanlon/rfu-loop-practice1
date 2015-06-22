c0257ca92c7e938d91f98808cdcf9a1fa881aec2
ahanlon <hanlonad27@gmail.com>
2015-06-22 12:01:23 -0600

    finished loop practice

---
+++ main.js
@@ -0,0 +1,32 @@
+
+for(var i = 0; i < 600; i += 100){
+	console.log(i);
+}
+
+for(var i = 1; i < 65; i *= 2){
+	console.log(i);
+}
+
+for(var i = 1; i < 4; i++){
+	for(var m = 1; m < 4; m++){
+		console.log(i);
+	}
+}
+
+for(var i = 0; i < 11; i += 2) {
+	console.log(i);
+}
+
+for(var i = 3; i < 16; i += 3) {
+	console.log(i);
+}
+
+for(var i = 9; i >= 0; i--) {
+	console.log(i);
+}
+
+for(var i = 1; i < 4; i++){
+	for(var m = 0; m < 4; m++){
+		console.log(m);
+	}
+}
